0f427c146a7956b4120f120696aca36c37683276
Chris Barranger <chrisbarranger@techie.com>
2023-11-12 21:06:37 -0400

    new

---
+++ Password Generator/pass_gen.py
@@ -0,0 +1,5 @@
+"""Password Generator"""
+import pass_gen_mod
+
+print(pass_gen_mod.password)
+print(pass_gen_mod.password1)
--- Password Generator/pass_gen_mod.py
+++ Password Generator/pass_gen_mod.py
@@ -1,9 +1,12 @@
+"""Password Generator"""
 import string
 import secrets
 alphabet = string.ascii_letters + string.digits
 while True:
-    password = ''.join(secrets.choice(alphabet) for i in range(10))
+    password = ''.join(secrets.choice(alphabet) for i in range(15))
     if (any(c.islower() for c in password)
             and any(c.isupper() for c in password)
             and sum(c.isdigit() for c in password) >= 3):
         break
+alphabet1 = string.ascii_letters + string.digits
+password1 = ''.join(secrets.choice(alphabet) for i in range(8))
