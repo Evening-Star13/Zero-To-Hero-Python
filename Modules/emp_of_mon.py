f69e9662c5336a6fc04b35aa3031954077447300
Chris Barranger <chrisbarranger@techie.com>
2023-11-13 00:02:26 -0400

    new

---
+++ Modules/emp_of_mon.py
@@ -0,0 +1,13 @@
+"""Employee Of The Month"""
+
+import emp_of_mon_mod
+
+work_hours = [
+    ('Abby', 100),
+    ('Billy', 236),
+    ('Joseph', 187),
+    ('Robby', 450),
+    ('Chris', 398),
+    ('Rebecca', 513)]
+
+print(emp_of_mon_mod.employee_check(work_hours))
